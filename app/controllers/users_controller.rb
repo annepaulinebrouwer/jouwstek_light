de0545928af73936b6395ffe1d8f625cab4234a2
Carolien Jonker <carolien.jonker@gmail.com>
2016-03-09 15:34:38 +0100

    created show of a garden

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
-  before_action :find_user, only: [ :show, :edit, :update, :destroy ]
 
   def show
     @user = User.find(params[:id])
   end
 
+  def new
+    @user = User.new
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :description, :photo, :photo_cache)
+  end
 end
