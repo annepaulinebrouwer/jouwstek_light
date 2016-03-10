f05548721ed0835a2dcfaf4928c14aacaf33f1d4
Paula Brouwer <annepaulinebrouwer@hotmail.com>
2016-03-10 15:38:37 +0100

    Add garden picture in assets-image file

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,6 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def home
-    # @garden = Garden.find(params[:id])
     @gardens = Garden.all
     @markers = Gmaps4rails.build_markers(@gardens) do |garden, marker|
       marker.lat garden.latitude
