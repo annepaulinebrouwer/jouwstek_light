4baa8fcc1c013b068104147f287b9008bb44300f
Paula Brouwer <annepaulinebrouwer@hotmail.com>
2016-03-10 12:24:26 +0100

    Puts map in div container and row

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    # @garden = Garden.find(params[:id])
     @gardens = Garden.all
     @markers = Gmaps4rails.build_markers(@gardens) do |garden, marker|
           marker.lat garden.latitude
