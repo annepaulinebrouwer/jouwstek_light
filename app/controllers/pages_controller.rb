63f8adb283a61a79c025906ece89481af4921599
Jojanneke Tamis <jojanneketamis@hotmail.com>
2016-03-11 16:14:56 +0100

    avatar on cards

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,10 +3,18 @@ class PagesController < ApplicationController
 
   def home
     @gardens = Garden.all
+    # @markers = Gmaps4rails.build_markers(@gardens) do |garden, marker|
+    #   marker.lat garden.latitude
+    #   marker.lng garden.longitude
+    # end
     @markers = Gmaps4rails.build_markers(@gardens) do |garden, marker|
-      marker.lat garden.latitude
-      marker.lng garden.longitude
-    end
+    garden_link = view_context.link_to garden.title, garden_path(garden)
+    marker.title garden.title
+    marker.infowindow "<h4><u>#{ garden_link }</u></h4>
+                       <i>#{ garden.address }</i>"
+    marker.lat garden.latitude
+    marker.lng garden.longitude
+end
     @users = User.all
     @user = User.first
   end
