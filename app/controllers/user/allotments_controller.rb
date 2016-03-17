37d0aa69ac8da4c8493bfa4b204989bd75ae4c6f
Paula Brouwer <annepaulinebrouwer@hotmail.com>
2016-03-17 17:22:34 +0100

    Working on the button to cancel an allotment

--- app/controllers/user/allotments_controller.rb
+++ app/controllers/user/allotments_controller.rb
@@ -1,7 +1,35 @@
 class User::AllotmentsController < ApplicationController
 
+   before_action :find_allotment, only: [ :cancel ]
+
   def index
     @allotments = current_user.allotments
+  end
+
+  def cancel
+    @allotment.status_request = "cancelled"
+     if @allotment.save
+      redirect_to user_allotments_index
+    else
+      redirect_to user_allotments_index
+      flash[:warning] = 'Canceling the garden didn work'
+    end
+  end
+
+  # def update
+  #   @garden.update!(garden_params)
+  # redirect_to garden_path(@garden)
+  # end
+
+  # def destroy
+  #   @garden.destroy!
+  # redirect_to user_path(@user_id)
+  # end
+  #
+private
+
+  def find_allotment
+    @allotment = Allotment.find(params[:id])
   end
 
 end
