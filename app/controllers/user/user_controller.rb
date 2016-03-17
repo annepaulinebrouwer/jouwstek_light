f6dfea1691b5b0e41aedfa9911311bda34f422a8
Carolien Jonker <carolien.jonker@gmail.com>
2016-03-17 17:22:02 +0100

    working on the edit user profile page

---
+++ app/controllers/user/user_controller.rb
@@ -0,0 +1,13 @@
+class UserController < ApplicationController
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user)
+  end
+
+end
