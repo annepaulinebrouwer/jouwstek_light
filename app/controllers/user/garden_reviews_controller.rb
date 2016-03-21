5a60df9d49dfb62693712d3575b69b8114e23ebb
Jojanneke Tamis <jojanneketamis@hotmail.com>
2016-03-21 16:38:26 +0100

    profile paths

--- app/controllers/user/garden_reviews_controller.rb
+++ app/controllers/user/garden_reviews_controller.rb
@@ -1,15 +1,19 @@
-class User::Garden_reviewsController < ApplicationController
+class User::GardenReviewsController < ApplicationController
 
    before_action :find_allotment, only: [ :cancel ]
 
   def new
-  @Garden_review = Garden_review.new
+  @Garden_review = GardenReview.new
   end
 
   def create
-    @garden_review = Garden_review.new(garden_arams)
-    @garden.garden_review = current_user
+    allotment = current_user.allotments.find(params[:allotment_id])
+    @garden_review = GardenReview.new(garden_params)
+    @garden_review.user = current_user
+    @garden_review.allotment = allotment
     @garden_review.save!
+
+    redirect_to user_allotments_path
   end
 
   def edit
@@ -23,7 +27,7 @@ class User::Garden_reviewsController < ApplicationController
 private
 
   def garden_params
-    params.require(:garden).permit(:description, :address, :available, :photo, :photo_cache)
+    params.require(:garden_review).permit(:description, :stars)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -26,6 +26,7 @@ Rails.application.routes.draw do
 
   namespace :owner do
     resource :garden
+    resource :profile, only: [:show, :edit, :update]
 
     resources :allotments, only: [:index] do
       member do
