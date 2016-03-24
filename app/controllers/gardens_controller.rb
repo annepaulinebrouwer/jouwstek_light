78845a3e41245b6a4a435176634f51fcdb83795f
Jojanneke Tamis <jojanneketamis@hotmail.com>
2016-03-24 17:51:19 +0100

    maps, sign up, avatar

--- app/controllers/gardens_controller.rb
+++ app/controllers/gardens_controller.rb
@@ -8,13 +8,13 @@ class GardensController < ApplicationController
     @markers = Gmaps4rails.build_markers(@gardens) do |garden, marker|
       garden_link = view_context.link_to garden.title, garden_path(garden)
       marker.title garden.title
-      marker.infowindow "<h4><u>#{ garden_link }</u></h4>
-                        <i>#{ garden.photo }</i><br>
-                         <i>#{ garden.address }</i><br>
-                         <i>#{ garden.description }</i>"
+      marker.infowindow "<h4><u class='marker-description'>#{ garden_link }</u></h4>
+                        <img src='#{ garden.photo }' class='marker-photo'/><br>
+                         <i class='marker-address'>#{ garden.address } </i><br>
+                         <i class='marker-description'>#{ garden.description }</i>"
       marker.lat garden.latitude
       marker.picture({
-         url: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png", # up to you to pass the proper parameters in the url, I guess with a method from device
+         url: "http://icons.iconarchive.com/icons/pixture/vegetable/32/Chinese-Spinach-icon.png", # up to you to pass the proper parameters in the url, I guess with a method from device
          width: 32,
          height: 32
       })
