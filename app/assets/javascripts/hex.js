cbfaa345f4541eb5ea5aa634f694197aebac6b59
Jojanneke Tamis <jojanneketamis@hotmail.com>
2016-03-25 16:53:55 +0100

    home en garden index

--- app/assets/javascripts/hex.js
+++ app/assets/javascripts/hex.js
@@ -105,6 +105,48 @@ $(document).ready(function() {
             }
 
         });
+
+           $('#hex8').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},2000);
+
+            }
+
+        });
+            $('#hex9').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},1100);
+
+            }
+
+        });
+             $('#hex10').each( function(i){
+
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+            /* If the object is completely visible in the window, fade it it */
+            if( bottom_of_window > bottom_of_object ){
+
+                $(this).animate({'opacity':'1'},3000);
+
+            }
+
+        });
+
+
     });
 
 });
