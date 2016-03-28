dad65c90d39f3d67d8ac1170b2649aeebb07f6da
Carolien Jonker <carolien.jonker@gmail.com>
2016-03-28 19:32:23 +0200

    perfectionized garden show

--- app/models/user_review.rb
+++ app/models/user_review.rb
@@ -1,6 +1,6 @@
 class UserReview < ActiveRecord::Base
   validates :description, :stars, presence: true
-  validates :owner_id, uniqueness: { scope: :user_id }
+  validates :owner_id, presence: true
   belongs_to :owner, class_name: 'User'
   belongs_to :user
 end
