8a60dea4c7d5c714f6bdfd1e12fe2acb40f3386b
Jojanneke Tamis <jojanneketamis@hotmail.com>
2016-03-29 15:22:53 +0200

    logo and counter

---
+++ app/assets/javascripts/scroll.js
@@ -0,0 +1,57 @@
+ $('a[href*="#"]:not([href="#"])').click(function() {
+   if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+     var target = $(this.hash);
+     target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+     if (target.length) {
+       $('html, body').animate({
+         scrollTop: target.offset().top
+       }, 1000);
+       return false;
+     }
+   }
+ });
+
+
+if($('.counters').length > 0) {
+  var helper = true
+  $(document).on('scroll', function() {
+    if (isScrolledIntoView('.counters') && helper) {
+      launchCount()
+    }
+  })
+
+  if (isScrolledIntoView('.counters') && helper) {
+    launchCount()
+  }
+}
+
+
+function launchCount() {
+  $('.count').each(function () {
+    helper = false
+
+      $(this).prop('Counter',0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 3000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+  });
+}
+
+function isScrolledIntoView(elem)
+{
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+    var elemBottom = elemTop + $elem.height();
+
+    return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
