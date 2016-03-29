bb715abdf4817e7f8d129248dbb42a2e4e40cd50
Carolien Jonker <carolien.jonker@gmail.com>
2016-03-29 13:28:36 +0200

    working on the error pages and made the nav bar better

--- app/models/garden_review.rb
+++ app/models/garden_review.rb
@@ -1,6 +1,6 @@
 class GardenReview < ActiveRecord::Base
-  validates :description, :stars, presence: true
-  validates :user_id, :allotment_id, presence: true
   belongs_to :user
   belongs_to :allotment
+  validates :description, :stars, presence: true
+  validates :user_id, :allotment_id, presence: true
 end
